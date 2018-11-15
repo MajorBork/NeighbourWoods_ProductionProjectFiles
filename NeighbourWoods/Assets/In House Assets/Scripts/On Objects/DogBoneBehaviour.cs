64db0d203f24682ac78bdbaa2a0d1c7fc353ad52
Zalron <33308793+Zalron@users.noreply.github.com>
2018-11-15 12:03:20 +1100

    Finished player house and beginning to add straight road
    Added dogbone script

---
+++ NeighbourWoods/Assets/In House Assets/Scripts/On Objects/DogBoneBehaviour.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Manager.Inventory;
+using Manager.UI;
+
+public class DogBoneBehaviour : MonoBehaviour
+{
+    public GameObject questObject;
+    public UIManager uiManager;
+    void Start () // Use this for initialization
+    {
+		
+	}
+	void Update () // Update is called once per frame
+    {
+		
+	}
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            uiManager.SendMessage("UpdateItemIcon");
+            questObject.SetActive(false);
+        }
+    }
+}
