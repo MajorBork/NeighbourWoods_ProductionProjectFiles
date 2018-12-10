388a929961af5ccbbc224a54e122ebc4d9177109
Zalron <33308793+Zalron@users.noreply.github.com>
2018-12-11 09:19:19 +1100

    Added more scripts to muffles and liea

---
+++ NeighbourWoods/Assets/In House Assets/Scripts/On Objects/LieaBehaviour.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LieaBehaviour : MonoBehaviour
+{
+    public GameObject Liea;
+	void Start () // Use this for initialization
+    {
+		
+	}
+	void Update () // Update is called once per frame
+    {
+		
+	}
+    public void LieaRetreat()
+    {
+        Liea.SetActive(false);
+    }
+}
---
+++ NeighbourWoods/Assets/In House Assets/Scripts/On Objects/MufflesBehaviour.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MufflesBehaviour : MonoBehaviour
+{
+    public GameObject Muffle;
+	void Start () // Use this for initialization
+    {
+
+    }
+	void Update () // Update is called once per frame
+    {
+
+    }
+    public void MufflesRetreat()
+    {
+        Muffle.SetActive(false);
+    }
+}
